69ea5caca8c31b5c193a1726d69fa17636d333a8
Jaqueline <jaqueline@schwencke.dk>
2024-02-07 19:58:47 +0100

    work on HttpRequest

--- src/main/java/dk/cphbusiness/demo03_httprequest/Parsers/HttpRequest.java
+++ src/main/java/dk/cphbusiness/demo03_httprequest/Parsers/HttpRequest.java
@@ -7,25 +7,18 @@ import java.util.StringTokenizer;
 
 public class HttpRequest {
 
-    String firstLine = "GET /pages/index.html HTTP/1.1";
-    String key;
-    String value;
+   // String firstLine = "GET /pages/index.html HTTP/1.1";
+    HashMap<String, String> headers = new HashMap<>();
+    StringTokenizer firstLineHeader;
 
-    StringTokenizer firstLineHeader = new StringTokenizer(firstLine , " ");
+    public HttpRequest() {
+    }
 
+    public void parse(String payload)
+    {
+        String[] a = payload.split(System.lineSeparator());
+        firstLineHeader = new StringTokenizer(a[0] , " ");
 
-    String header = "Host: www.example.com\n" +
-            "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3\n" +
-            "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\n" +
-            "Accept-Language: en-US,en;q=0.5\n" +
-            "Accept-Encoding: gzip, deflate, br\n" +
-            "Connection: keep-alive\n" +
-            "Upgrade-Insecure-Requests: 1";
-
-
-    HashMap<String, String> HttpKeyAndValue = new HashMap<>();
-
-
-
+    }
 
 }
